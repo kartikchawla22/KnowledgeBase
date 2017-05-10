9705c5548a7b46a0c6d622b04b0b2f8baa96bed6
hh <y@y.com>
2017-05-10 20:31:36 +0530

    Validations

--- KnowledgeBaseAngular2/src/app/users/users.component.ts
+++ KnowledgeBaseAngular2/src/app/users/users.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormGroup, FormBuilder, FormControl, FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+public UserForm : FormGroup;
+  constructor(public FormValidation : FormBuilder) {
 
-  constructor() { }
+this.UserForm = this.FormValidation.group({
+Username :[null, [Validators.required, Validators.pattern('[a-z]|[A-Z]')]],
+Password : [null, [Validators.required]]
+
+});
+
+
+   }
 
   ngOnInit() {
   }
