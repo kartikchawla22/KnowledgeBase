1b380faf66b101943ac6f67da79d29f4d99ddcb5
Divya <divya24_jain@yahoo.com>
2017-05-10 15:57:37 +0530

    ndivya

--- KnowledgeBaseAngular2/src/app/app.module.ts
+++ KnowledgeBaseAngular2/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MainbodyComponent } from './mainbody/mainbody.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainbodyComponent
+    MainbodyComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
