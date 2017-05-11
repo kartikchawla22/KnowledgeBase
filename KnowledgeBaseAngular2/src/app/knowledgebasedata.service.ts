ad9301e83cc6af3d79cf6720f57eae4acf306799
Divya <divya24_jain@yahoo.com>
2017-05-11 15:45:37 +0530

    test10

--- KnowledgeBaseAngular2/src/app/knowledgebasedata.service.ts
+++ KnowledgeBaseAngular2/src/app/knowledgebasedata.service.ts
@@ -12,10 +12,6 @@ public name1:string="";
   getdetails(): Observable<any>{
     console.log(this.name1);
     var a:string='https://api.github.com/users/';
-    var b:string=this.name1.trim();
-    a=a+b;
-    //a.replace('%20', "");
-    console.log(a);
     return this.httpService.get(a).map(
       data => data.json()
     );
@@ -24,16 +20,7 @@ public name1:string="";
   {
      this.name1=dat;
   }
-  getprodat(name_pro):Observable<any>
-  {
-   
-    var a:string='https://api.github.com/search/repositories?q=';
-    var b:string=name_pro.trim();
-    a=a+b;
-    return this.httpService1.get(a).map(
-      data1 => data1.json()
-    );
-  }
+
      private handleError(error: any) {
 
         if (error.status === 401) {
